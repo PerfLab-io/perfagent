2e2e08e47168e39dfa10be26eda79b97a2440725
Vinicius Dallacqua <dev.webx@gmail.com>
2025-04-03 10:49:54 +0200

    Add Langfuse

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,9 @@
 import { registerOTel } from '@vercel/otel';
+import { LangfuseExporter } from 'langfuse-vercel';
 
 export function register() {
-	registerOTel('next-app');
+	registerOTel({
+		serviceName: 'perf-agent',
+		traceExporter: new LangfuseExporter(),
+	});
 }
