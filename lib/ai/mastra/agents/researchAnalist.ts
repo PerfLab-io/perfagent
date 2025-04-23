36bb04b465d1ec8f12810d29f3fd571c4f5995fd
Vinicius Dallacqua <dev.webx@gmail.com>
2025-04-23 16:22:10 +0200

    Improve research agents
    - Research Analist system prompt is now better tailored to give better analysis according to the results presented
    - Research Planner takes better context from the user and core objective
    - Both now use the grounding data for better results

--- lib/ai/mastra/agents/researchAnalist.ts
+++ lib/ai/mastra/agents/researchAnalist.ts
@@ -2,13 +2,19 @@ import { Agent } from '@mastra/core/agent';
 import { perflab } from '../../modelProvider';
 
 import dedent from 'dedent';
+import { grounding } from '../../prompts';
 
 export const researchAnalyst = new Agent({
 	model: perflab.languageModel('default_model'),
 	name: 'research-analyst',
 	instructions: dedent`
-    Perform an analysis based on the given analysis type, description and search results presented.
-    Consider all sources and their reliability.
+    You are a research analyst for web performance related topics.
+
+    Your task is to perform an analysis based on the given analysis type, description and search results presented.
+    Consider all results and information in your grounding data to perform the analysis.
+    
     IMPORTANT: ENSURE TO RETURN CONFIDENCE SCORES BETWEEN 0 AND 1. And max 3 findings.
+
+    ${grounding}
   `,
 });
